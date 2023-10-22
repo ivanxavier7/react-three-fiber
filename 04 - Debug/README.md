198128ea2dc405942d9148fa8aeafe6de601dfc2
Ivan Xavier <ivanxavier@ua.pt>
2023-10-22 22:34:24 +0100

    Debug( Console, GUI, Monitoring )
     - Trigger Three.js problems in the console, GUI and Monitor the resources

--- 04 - Debug/README.md
+++ 04 - Debug/README.md
@@ -1,2 +1,63 @@
 # Debug
 
+Use React Developer Tools extension for the browser.
+
+1. StrictMode
+2. Leva
+
+## 1 - StrictMode
+
+Warn about some problems:
+
+* Infinite render loop
+* Forgotten useEffect dependencies
+* Unused imports
+* Deprecated pratices
+
+``` javascript
+import { StrictMode } from 'react'
+
+const root = ReactDOM.createRoot(document.querySelector('#root'))
+
+root.render(
+    <StrictMode>
+        <Canvas
+            camera={ {
+                fov: 45,
+                near: 0.1,
+                far: 200,
+                position: [ - 4, 3, 6 ]
+            } }
+        >
+            <Experience />
+        </Canvas>
+    </StrictMode>
+)
+```
+
+## 2 - Leva
+
+* [Leva Documentation](https://github.com/pmndrs/leva)
+* [Leva Configuration](https://github.com/pmndrs/leva/blob/main/docs/configuration.md)
+
+``` bash
+npm install leva@0.9
+```
+
+## 3 - Monitoring
+
+* [R3F-Perf Docs](https://github.com/utsuboco/r3f-perf)
+
+`r3f-perf` Monitors resource consumption on the graphics card, processor, counts FPS, calls and total triangles
+
+``` bash
+npm install r3f-perf@7.1
+```
+
+``` javascript
+import { Perf } from 'r3f-perf'
+
+<Perf
+    position="top-left"
+/>
+```
--- 04 - Debug/src/Experience.jsx
+++ 04 - Debug/src/Experience.jsx
@@ -1,17 +1,70 @@
 import { OrbitControls } from '@react-three/drei'
 
+import { button, useControls } from 'leva'
+
+import { Perf } from 'r3f-perf'
+
 export default function Experience()
 {
-    return <>
+    /*
+    const { position, color, visible } = useControls({ 
+        position:
+        {
+            value: { x: -2, y: 0 },
+            min: -4,
+            max: 4,
+            step: 0.01,
+            joystick: 'invertY'
+        },
+        color: '#854381',
+        visible: true,
+        myInterval:
+        {
+            min: 0,
+            max: 10,
+            value: [4, 5]
+        }
+    })
+    */
+const { position, color, visible, perfVisible } = useControls('Folder Name',
+    { 
+        position:
+        {
+            value: { x: -2, y: 0 },
+            min: -4,
+            max: 4,
+            step: 0.01,
+            joystick: 'invertY'
+        },
+        color: '#854381',
+        visible: true,
+        myInterval:
+        {
+            min: 0,
+            max: 10,
+            value: [ 4, 5 ]
+        },
+        clickMe: button(() =>
+        {
+            console.log('Sphere clicked')
+        }),
+        choice: { options: ['a', 'b', 'c'] },
+        perfVisible: true,
+    })
 
+    return <>
+        { perfVisible && <Perf position="top-left" /> }
         <OrbitControls makeDefault />
 
         <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
         <ambientLight intensity={ 0.5 } />
 
-        <mesh position-x={ - 2 }>
+        <mesh
+            visible={ visible }
+            position={ [position.x, position.y, 0] }
+        >
             <sphereGeometry />
-            <meshStandardMaterial color="#854381" />
+            <meshStandardMaterial color={ color } />
         </mesh>
 
         <mesh position-x={ 2 } scale={ 1.5 }>
