fa3f754ca7abf98b879d01fc054367f82eee8e07
Ivan Xavier <ivanxavier@ua.pt>
2023-10-23 21:59:56 +0100

    Update Placeholder.jsx

--- 06 - Load Models/src/Placeholder.jsx
+++ 06 - Load Models/src/Placeholder.jsx
@@ -1,6 +1,6 @@
 export default function Placeholder(props)
 {
-    return <mesh { ...props /* Retreive all the properties defined in the mesh instatiation*/>}>
+    return <mesh { ...props /* Retreive all the properties defined in the mesh instatiation*/}>
         <boxGeometry
             args={ [ 1, 1, 1, 2, 2, 2] }
         />
