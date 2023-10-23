f86ade492541334dfdea4044c5921943dcbf6ed0
Ivan Xavier <ivanxavier@ua.pt>
2023-10-23 21:21:34 +0100

    Loading Models using normal loader and Draco compression

--- 06 - Load Models/README.md
+++ 06 - Load Models/README.md
@@ -1,2 +1,62 @@
 # Load Models
 
+1. Loader and Draco
+
+``` javascript
+import { useLoader } from '@react-three/fiber'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader'
+
+{/*
+const model = useLoader(
+    GLTFLoader, 
+    './hamburger.glb'
+)
+*/}
+
+const model = useLoader(
+    GLTFLoader,
+    './hamburger-draco.glb',
+    (loader) =>
+    {
+        const dracoLoader = new DRACOLoader()
+        dracoLoader.setDecoderPath('./draco/')
+        loader.setDRACOLoader(dracoLoader)
+    })
+
+<primitive
+    object={ model.scene }
+    scale={ 0.35 }
+    position-y={ -1 }
+/>
+```
+
+2. Lazy Loader
+
+``` javascript
+
+```
+
+3. Drei
+
+``` javascript
+
+```
+
+4. Multiple instances
+
+``` javascript
+
+```
+
+5. GLTF Component
+
+``` javascript
+
+```
+
+6. Animations
+
+``` javascript
+
+```
--- 06 - Load Models/src/Experience.jsx
+++ 06 - Load Models/src/Experience.jsx
@@ -1,10 +1,29 @@
 import { OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
+import { useLoader } from '@react-three/fiber'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader'
 
 export default function Experience()
 {
-    return <>
+    {/*
+    const model = useLoader(
+        GLTFLoader, 
+        './hamburger.glb'
+    )
+    */}
 
+    const model = useLoader(
+        GLTFLoader,
+        './hamburger-draco.glb',
+        (loader) =>
+        {
+            const dracoLoader = new DRACOLoader()
+            dracoLoader.setDecoderPath('./draco/')
+            loader.setDRACOLoader(dracoLoader)
+        })
+
+    return <>
         <Perf position="top-left" />
 
         <OrbitControls makeDefault />
@@ -12,20 +31,15 @@ export default function Experience()
         <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
         <ambientLight intensity={ 0.5 } />
 
-        <mesh castShadow position-x={ - 2 }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
-        </mesh>
-
-        <mesh castShadow position-x={ 2 } scale={ 1.5 }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
-        </mesh>
+        <primitive
+            object={ model.scene }
+            scale={ 0.35 }
+            position-y={ -1 }
+        />
 
         <mesh receiveShadow position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
             <planeGeometry />
-            <meshStandardMaterial color="greenyellow" />
+            <meshStandardMaterial color="#115014" />
         </mesh>
-
     </>
 }
